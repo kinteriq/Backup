f7c9fbbd289aa89a11a6e967dea0c4210d76b03b
Kint Eriq <kinteriq@gmail.com>
2019-08-08 15:31:50 +0500

    Divide functionality in commands module.

--- backup/commands.py
+++ backup/commands.py
@@ -1,10 +1,25 @@
+#     Backup is a tool which makes day-to-day backups easier.
+#
+#     Copyright (C) 2019  kinteriq
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import sys
 
-_ERROR_MESSAGE = {
-    'wrong_command': 'There is no such command: ',
-}
+import error
 
-_AVAILABLE_COMMANDS = {
+_COMMANDS = {
     'create': None,
     'update': None,
     'delete': None,
@@ -13,13 +28,18 @@ _AVAILABLE_COMMANDS = {
 }
 
 
-def check_args(args: tuple) -> bool:
-    if _shortcut_call(args):
+def check_args(args) -> bool:
+    if _empty(args):
+        print(error.NoArgs())
+        sys.exit()
+
+    if _shortcut_name(args):
         run_backup(shortcut=args[0])
         return False
 
-    if not _is_valid_command(args):
-        raise Exception(_ERROR_MESSAGE['wrong_command'])
+    if not _valid_command(args):
+        print(error.InvalidCommand())
+        sys.exit()
 
     return True
 
@@ -28,18 +48,27 @@ def execute_command() -> str:
     args = sys.argv[1:]
     if check_args(args):
         command, params = args[0], args[1:]
-        output = _AVAILABLE_COMMANDS[command](params)
+        output = _COMMANDS[command](params)
         return output
 
 
-def run_backup(shortcut: str):
+def run_backup(shortcut):
     pass
 
 
-def _shortcut_call(args: tuple):
-    pass
-
-
-def _is_valid_command(args: tuple):
+def _empty(args) -> bool:
     if not args:
-        return False
+        return True
+    return False
+
+
+def _shortcut_name(args) -> bool:
+    if len(args) == 1 and args[0] in ['NAME']:
+        return True
+    return False
+
+
+def _valid_command(args) -> bool:
+    if args[0] in _COMMANDS:
+        return True
+    return False
