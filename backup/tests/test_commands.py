08572729b428479648f06806de3593c336c54121
Kint Eriq <kinteriq@gmail.com>
2019-08-08 20:41:02 +0500

    Add test_execute_create_command().

--- backup/tests/test_commands.py
+++ backup/tests/test_commands.py
@@ -1,7 +1,9 @@
 import pytest
+import sys
 
 from .context import backup
-from backup.commands import is_command
+from backup.commands import _is_command, execute_command
+from backup.error import MSG as message
 
 test_args_shortcut_name = ['NAME']
 test_args_valid_command = ['create', 'NAME', 'from/path', 'to/path']
@@ -11,15 +13,24 @@ test_args_invalid_command = ['messed', 'up']
 class TestCheckArgs():
     def test_got_a_shortcut(self):
         with pytest.raises(SystemExit):
-            is_command(test_args_shortcut_name)
+            _is_command(test_args_shortcut_name)
 
     def test_got_valid_command(self):
-        assert is_command(test_args_valid_command)
+        assert _is_command(test_args_valid_command)
 
     def test_got_invalid_command(self):
-        with pytest.raises(Exception):
-            is_command(test_args_invalid_command)
+        with pytest.raises(SystemExit) as e:
+            _is_command(test_args_invalid_command)
+        assert message['invalid_cmd'] in e.exconly()
 
     def test_got_empty_line(self):
-        with pytest.raises(Exception):
-            is_command([])
+        with pytest.raises(SystemExit) as e:
+            _is_command([])
+        assert message['no_args'] in e.exconly()
+
+
+def test_execute_create_command(monkeypatch):
+    monkeypatch.setattr(
+        sys, 'argv', ['backup.py', 'create', 'NAME', 'from/path', 'to/path'])
+    output = execute_command()
+    assert output == f'Shortcut is created: "NAME".'
