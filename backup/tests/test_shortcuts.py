2ec7499b9cf924eb46af95bed28e260c8843213f
Kint Eriq <kinteriq@gmail.com>
2019-08-10 11:47:20 +0500

    Convert unittest cases to pytest.

--- backup/tests/test_shortcuts.py
+++ backup/tests/test_shortcuts.py
@@ -1,31 +1,28 @@
-import unittest
-from unittest.mock import patch
+import pytest
 
 from backup.shortcuts import Shortcuts
 
 
-class TestShortcuts(unittest.TestCase):
-    def setUp(self):
-        self.shortcuts = Shortcuts({})
-        self.shortcut = 'test'
-        self.result = self.shortcuts.create(
-            [self.shortcut, 'from/path', 'to/paths'])
+class TestShortcuts():
+    @classmethod
+    def setup_class(self):
+        self.data = {}
+        self.name = 'NAME'
+        self.result = Shortcuts.create(self.data,
+            [self.name, 'from/path', 'to/paths'])
 
     def test_shortcut_is_created(self):
-        expected_result = f'Shortcut is created: "{self.shortcut}".'
-        self.assertIn(self.shortcut, self.shortcuts.data)
-        self.assertEqual(self.result, expected_result)
+        expected_result = f'Shortcut is created: "{self.name}".'
+        assert self.name in self.data
+        assert self.result == expected_result
 
     def test_show_shortcut(self):
-        result = self.shortcuts.show([self.shortcut])
-        expected_result = f'{self.shortcut}:\n' +\
-            str(self.shortcuts.data[self.shortcut])
-        self.assertEqual(result, expected_result)
+        result = Shortcuts.show(self.data, [self.name])
+        expected_result = f'{self.name}:\n' +\
+            str(self.data[self.name])
+        assert result == expected_result
 
-    user_input = ['changed/path', '']
-
-    @patch('builtins.input', side_effect=user_input)
-    def test_update_shortcut(self, user_input):
-        self.shortcuts.update([self.shortcut])
-        self.assertEqual(self.shortcuts.data[self.shortcut]['source'],
-                         'changed/path')
+    # def test_update_shortcut(self, monkeypatch):
+    #     monkeypatch('builtins.input', ['changed/path', ''])
+    #     self.shortcuts.update([self.name])
+    #     assert self.shortcuts.data[self.name]['source'] == 'changed/path'
