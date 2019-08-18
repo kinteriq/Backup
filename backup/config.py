9db34234fbff2c1b66786c7361ecdd0692f6c056
Kint Eriq <kinteriq@gmail.com>
2019-08-18 14:47:49 +0500

    Add license info.

--- backup/config.py
+++ backup/config.py
@@ -1,3 +1,20 @@
+#     Backup is a tool which makes day-to-day backups easier.
+#
+#     Copyright (C) 2019  kinteriq
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 
 # This is the path to .json file where all shortcuts will be saved
