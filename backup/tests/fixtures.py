a908fa2953d711c79e5f028d913c80904e8a1517
Kint Eriq <kinteriq@gmail.com>
2019-08-18 14:46:30 +0500

    Add DATA to fixtures.

--- backup/tests/fixtures.py
+++ backup/tests/fixtures.py
@@ -2,12 +2,32 @@ import pytest
 import os
 import json
 
+from backup import file_handle
+
+
+testsource1 = os.path.join(os.getcwd(), 'testsource1')
+testdestination1 = os.path.join(os.getcwd(), 'testdestination1')
+testsource2 = os.path.join(os.getcwd(), 'testsource2')
+testdestination2 = os.path.join(os.getcwd(), 'testdestination2')
+
+
+DATA = {
+    'TEST1': {
+        'source': testsource1,
+        'destination': [testdestination1]
+    },
+    'TEST2': {
+        'source': testsource2,
+        'destination': [testdestination2]
+    },
+}
+
 
 @pytest.fixture
 def mock_filepath():
     filepath = os.path.join(os.getcwd(), 'test_shortcuts.json')
     with open(filepath, 'w') as file:
-        file.write(json.dumps({}))
+        file.write(json.dumps(DATA))
     yield filepath
     os.remove(filepath)
 
