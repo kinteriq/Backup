98bde2edc7fdc178dbc98fc5424e3c31f7b6e850
kinteriq <kinteriq@gmail.com>
2019-10-02 19:35:05 +0500

    Finish test_commands.py

--- backup/tests/test_commands.py
+++ backup/tests/test_commands.py
@@ -5,10 +5,11 @@ import sys
 from commands import execute_command
 import outputs
 from unittest.mock import patch
-from constants import (DB, SHORTCUT_NAMES, VALID_ARGS_WITH_MOCK_DB,
-                       CreateCmd, ShowallCmd, RunbackupCmd)
+from constants import (DB, VALID_ARGS_WITH_MOCK_DB,
+                       CreateCmd, RunbackupCmd)
 
 
+@pytest.mark.commands
 @pytest.mark.parametrize('args', VALID_ARGS_WITH_MOCK_DB)
 def test_execute_commands(monkeypatch, mock_fields_db, args):
     monkeypatch.setattr('sys.argv', args)
@@ -19,3 +20,19 @@ def test_execute_commands(monkeypatch, mock_fields_db, args):
         with patch('builtins.input', side_effect=['', DB['third_destination']]):
             assert execute_command(datapath=mock_fields_db) is None,\
             'execute_command did not execute correctly'
+
+
+@pytest.mark.commands
+def test_execute_create_cmd(monkeypatch, empty_db_cursor, DB_PATH):
+    monkeypatch.setattr('sys.argv', CreateCmd().args())
+    assert execute_command(datapath=DB_PATH) is None,\
+        'execute_command (create) did not execute correctly'
+
+
+@pytest.mark.commands
+def test_runbackup_cmd(monkeypatch, mock_fields_db):
+    monkeypatch.setattr('sys.argv', RunbackupCmd().args())
+    with pytest.raises(SystemExit) as e:
+        execute_command(datapath=mock_fields_db)
+    assert e.exconly().endswith(outputs.PROGRAM_END),\
+        'execute_command (runbackup) did not execute correctly'
