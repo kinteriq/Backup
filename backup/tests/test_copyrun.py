9b5c17fb828f3ca79ed568b272cc5280c3c42586
kinteriq <kinteriq@gmail.com>
2019-10-07 17:38:26 +0500

    Finish test_copy_with_all_replacements.

--- backup/tests/test_copyrun.py
+++ backup/tests/test_copyrun.py
@@ -21,28 +21,32 @@ def test_copyall_ok(mock_fields_db):
         'Did not copied correctly to the second destination'
 
 
-def _transfer_files():
-    shutil.copytree(CREATE_1.source, CREATE_1.destinations[0])
-    transferred_files = set()
-    for this_dir, _, filenames in os.walk(CREATE_1.source):
-        for file in filenames:
-            transferred_files.add(os.path.join(this_dir, file))
-    return transferred_files
-
-
-@pytest.mark.xfail(reason='WIP', strict=True)
 def test_copy_with_all_replacements(monkeypatch, mock_fields_db):
     transfer = _transfer_files()
     monkeypatch.setattr('sys.stdin', StringIO('all'))
     with mock.patch('sys.stdout', new=StringIO()) as mock_out:
         copyrun.call([CREATE_1.name], mock_fields_db)
-    assert transferred < set(mock_out.getvalue().split())
+    assert transfer < set(mock_out.getvalue().split())
 
 
-@pytest.mark.xfail(reason='WIP', strict=True)
+@pytest.mark.xfail(strict=True)
 def test_copy_without_any_replacements(monkeypatch, mock_fields_db):
-    transferred = _transfer_files()
-    monkeypatch.setattr('sys.stdin', StringIO('nothing'))
     with mock.patch('sys.stdout', new=StringIO()) as mock_out:
+        monkeypatch.setattr('sys.stdin', StringIO('nothing'))
         copyrun.call([CREATE_1.name], mock_fields_db)
-    assert transferred < set(mock_out.getvalue().split())
+    assert len(mock_out.getvalue().split()) == 10
+
+
+def _transfer_files():
+    shutil.copytree(CREATE_1.source, CREATE_1.destinations[0])
+    transferred_files = set()
+    for this_dir, _, filenames in os.walk(CREATE_1.source):
+        for file in filenames:
+            if this_dir != CREATE_1.source:
+                filepath = os.path.join(CREATE_1.destinations[0],
+                                        os.path.split(this_dir)[1],
+                                        file)
+            else:
+                filepath = os.path.join(CREATE_1.destinations[0], file)
+            transferred_files.add('-->' + filepath)
+    return transferred_files
